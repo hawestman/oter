5137a818d95d858afed0de312467261e203c2701
Hallvard Westman <hallvard@westman.no>
2015-04-10 09:51:44 +0200

    updated shema

--- oter/oter-ejb/src/main/java/no/sismo/oter/ejb/model/schema.sql
+++ oter/oter-ejb/src/main/java/no/sismo/oter/ejb/model/schema.sql
@@ -1,3 +1,7 @@
+
+  CREATE KEYSPACE oter
+  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+
   create table service(
       numberId text,
       provider text,
@@ -5,7 +9,7 @@
       data text,
       created timestamp,
       lastChanged timestamp,
-      PRIMARY KEY (fonr, provider, service)
+      PRIMARY KEY (numberId, provider, service)
   	);
 
 insert into oter.service(fonr,provider,service,data,created,lastChanged) values (10049047105,'kartverket','hentEiendom','<xml>somexml</xml>',dateof(now()),dateof(now()));
